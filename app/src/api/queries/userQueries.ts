230392694a43d7fafafffce52fff2bbb032a17a6
andonynt <andonynunezt@gmail.com>
2025-08-26 21:11:44 -0500

    add query keys

--- app/src/api/queries/userQueries.ts
+++ app/src/api/queries/userQueries.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUserFn } from '../repositories/userRepositories';
+import { QUERY_KEYS } from '@/constants/queryKeys';
 
 const useUser = () => {
   return useQuery({
-    queryKey: ['user'],
+    queryKey: [QUERY_KEYS.users],
     queryFn: getUserFn,
   });
 };
---
+++ app/src/constants/queryKeys.ts
@@ -0,0 +1,5 @@
+const QUERY_KEYS = {
+  users: 'users',
+};
+
+export { QUERY_KEYS };
